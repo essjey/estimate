6177f601d0bddcfbe50acfb165d22459cdbe1365
Scott Johnson <scott.johnson@theygsgroup.com>
2022-03-01 08:06:17 -0500

    added styles & icon for estimate element

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
-import EstimateForm from './components/EstimateForm.vue'
+import EstimateForm from "./components/EstimateForm.vue";
 </script>
 
 <template>
-  <!-- <header>
+    <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,44 +17,46 @@ import EstimateForm from './components/EstimateForm.vue'
     
   </main> -->
 
-  <EstimateForm />
-
+    <EstimateForm />
 </template>
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
+
+body {
+    place-items: top center;
+}
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  font-weight: normal;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
 }
 
 header {
-  line-height: 1.5;
+    line-height: 1.5;
 }
-
 
 a,
 .green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+    text-decoration: none;
+    color: hsla(160, 100%, 37%, 1);
+    transition: 0.4s;
 }
 
 @media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+    a:hover {
+        background-color: hsla(160, 100%, 37%, 0.2);
+    }
 }
 
 @media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
+    body {
+        display: flex;
+        place-items: center;
+    }
 
-  /* #app {
+    /* #app {
     display: grid;
     grid-template-columns: 1fr 1fr;
     padding: 0 2rem;
